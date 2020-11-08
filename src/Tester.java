87cf6d82385d2ada734569f6be1749e1f6c3bee0
Gabriel Jospin <gabriel.jospin@usp.br>
2020-11-08 17:41:11 -0300

    Final version

--- src/Tester.java
+++ src/Tester.java
@@ -1,70 +1,230 @@
+import java.util.Scanner;
+
 public class Tester {
-    public static void main(String[] args) {
 
-        final int TESTEFOR = 5;
+    static Scanner input = new Scanner(System.in);
 
-        long start = System.currentTimeMillis();
-        Reader reader = new Reader();
-        Vector[] test = reader.read(TESTEFOR);
-        long readTime = System.currentTimeMillis() - start;
+    public static void main(String[] args) throws Exception {
 
-        for(int i = 0; i<test.length;i++){
-            if(test[i].isNotEmpty())
-                test[i].print();
+
+
+        System.out.println("Welcome to IAA Work");
+        System.out.println("Chose the test that you want:");
+        System.out.println("1- Insertion Sort");
+        System.out.println("2- Recursive Insertion Sort");
+        System.out.println("3- Binary Insertion Sort");
+        System.out.println("4- Recursive Binary Insertion Sort");
+        System.out.println("5- FullHouse (Don't choice this for now)");
+
+        int tester = input.nextInt();
+
+        switch (tester){
+            case 1:
+                InsertionTest();
+                break;
+            case 2:
+                RecursiveInsertionTest();
+                break;
+            case 3:
+                BinaryTest();
+                break;
+            case 4:
+                RecursiveBinaryTest();
+                break;
+            case 5:
+                InsertionTest();
+                RecursiveInsertionTest();
+                BinaryTest();
+                RecursiveBinaryTest();
+                break;
+            default:
+                throw new Exception("Error: Not found the test that choice ");
         }
 
-        long firstPrintTime = System.currentTimeMillis() - readTime;
 
-        for(int i = 0; i <test.length; i++){
-            try{
-                test[i].binaryInsertionSort(test[i],test[i].length);
 
-            }catch (Exception e){
+    }
+
+    private static int SelectArchive() throws Exception{
+        System.out.println("insert the archive's name to read");
+        System.out.println("available archives:");
+        System.out.println("vetor5.txt");
+        System.out.println("vetor10.txt");
+        System.out.println("vetor50.txt");
+        System.out.println("vetor100.txt");
+        System.out.println("vetor500.txt");
+        System.out.println("vetor1000.txt");
+        System.out.println("vetor5000.txt");
+        System.out.println("vetor10000.txt");
+        String arq = input.next();
+        int i = -1;
+        switch (arq){
+            case "vetor5.txt":
+                i = 0;
+                break;
+            case "vetor10.txt":
+                i = 1;
+                break;
+            case "vetor50.txt":
+                i = 2;
+                break;
+            case "vetor100.txt":
+                i = 3;
+                break;
+            case "vetor500.txt":
+                i = 4;
+                break;
+            case "vetor1000.txt":
+                i = 5;
+                break;
+            case "vetor5000.txt":
+                i = 6;
+                break;
+            case "vetor10000.txt":
+                i = 7;
+                break;
+            default:
+                throw new Exception("archive not found");
+        }
+        return i;
+    }
+
+
+    private static void InsertionTest() throws Exception {
+        int i = SelectArchive();
+        final Reader reader = new Reader();
+        final int TESTEFOR[] = {5,10,50,100,500,1000,5000,10000} ;
+
+        Vector[] test = reader.read(TESTEFOR[i]);
+
+        long timeIn = System.nanoTime();
+
+        for(int j = 0; j <test.length; j++) {
+            try {
+                //test[j].print();
+                //System.out.println();
+                test[j].insertionSort(test[j], test[j].length);
+                //test[j].print();
+                //System.out.println();
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        long insertionSortMediumTime = (System.currentTimeMillis() - firstPrintTime);
+        long timeOut = System.nanoTime();
 
-        for(int i = 0; i<test.length;i++){
-            if(test[i].isNotEmpty())
-                test[i].print();
+        System.out.printf("For %d arrays, the system delay %d  \n ",TESTEFOR[i],(timeOut-timeIn)/50);
+
+
+
+
+
+    }
+
+
+    private static void RecursiveInsertionTest() throws Exception {
+        int i = SelectArchive();
+
+        final Reader reader = new Reader();
+        final int TESTEFOR[] = {5,10,50,100,500,1000,5000,10000} ;
+
+        Vector[] test = reader.read(TESTEFOR[i]);
+
+        long timeIn = System.nanoTime();
+
+        for(int j = 0; j <test.length; j++) {
+            try {
+                //test[j].print();
+                //System.out.println();
+                test[j].insertionSortR(test[j], test[j].length);
+                //test[j].print();
+                //System.out.println();
+
+            }catch (StackOverflowError e) {
+                System.out.println("Stack Over flow");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
-        long secondPrintTime = System.currentTimeMillis() - insertionSortMediumTime;
+        long timeOut = System.nanoTime();
 
-        for(int i = 0; i < test.length;i++){
-            if(test[i].isNotEmpty())
-                try{
-                    System.out.print(Vector.binarySearch(test[i],test[i].length,test[i].vector[i%TESTEFOR]));
-                }catch (Exception e){
-                    System.out.print(-1);
-                }
+        System.out.printf("For %d arrays, the system delay %d nanoseconds \n",TESTEFOR[i],(timeOut-timeIn)/50);
 
+
+
+
+
+    }
+
+
+    private static void BinaryTest() throws Exception {
+        int i = SelectArchive();
+
+        final Reader reader = new Reader();
+        final int TESTEFOR[] = {5,10,50,100,500,1000,5000,10000} ;
+
+        Vector[] test = reader.read(TESTEFOR[i]);
+
+        long timeIn = System.nanoTime();
+
+        for(int j = 0; j <test.length; j++) {
+            try {
+                //test[j].print();
+                //System.out.println();
+                test[j].binaryInsertionSort(test[j], test[j].length);
+                //test[j].print();
+                //System.out.println();
+
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
         }
 
-        System.out.println();
+        long timeOut = System.nanoTime();
 
-        long binarySearchMediumTime = System.currentTimeMillis() - secondPrintTime;
+        System.out.printf("For %d arrays, the system delay %d nanoseconds \n",TESTEFOR[i],(timeOut-timeIn)/50);
 
-        for(int i = 0; i<test.length;i++){
-            if(test[i].isNotEmpty())try{
-                Vector.binarySearchR(test[i],test[i].vector[i%TESTEFOR],0,test[i].length-1);
 
+
+
+    }
+
+
+    private static void RecursiveBinaryTest() throws Exception {
+        int i = SelectArchive();
+
+        final Reader reader = new Reader();
+        final int TESTEFOR[] = {5,10,50,100,500,1000,5000,10000} ;
+
+        Vector[] test = reader.read(TESTEFOR[i]);
+
+        long timeIn = System.nanoTime();
+
+        for(int j = 0; j <test.length; j++) {
+            try {
+                //test[j].print();
+                //System.out.println();
+                test[j].binaryInsertionSortR(test[49], 1);
+
+            } catch (StackOverflowError e) {
+                System.out.println("Stack Over flow");
             }catch (Exception e){
-                System.out.print(-1);
+                e.printStackTrace();
+            }finally{
+                //test[j].print();
+                //System.out.println();
             }
 
+
+
         }
+        long timeOut = System.nanoTime();
+        System.out.printf("For %d arrays, the system delay %d nanoseconds \n",TESTEFOR[i],(timeOut-timeIn)/50);
 
 
-        long binarySearchMediumTimeR = System.currentTimeMillis() - binarySearchMediumTime;
 
-
-        System.out.printf("\nRead delay: %d ms \n" , readTime);
-        System.out.printf("binary delay: %d ms " , insertionSortMediumTime);
-        System.out.printf("binarySerarch delay: %d ms \n" , binarySearchMediumTime);
-        System.out.printf("binarySerarchR delay: %d ms \n" , binarySearchMediumTimeR);
 
 
     }
--- src/Vector.java
+++ src/Vector.java
@@ -1,4 +1,3 @@
-import javax.swing.plaf.synth.SynthEditorPaneUI;
 
 public class Vector {
     int length;
@@ -14,11 +13,11 @@ public class Vector {
     Vector(double[] vector, int length){
         this.length = length;
         this.vector = vector;
-        this.name = String.format("Vetor de %d", vector.length);
+        this.name = String.format("Vector");
     }
 
     public void print(){
-        System.out.printf("Imprimindo %s de tamanho %d:( ", name, length);
+        System.out.printf("Printing %s of size %d:( ", name, length);
         for(int i = 0; i < length; i++){
                 System.out.printf("%.2f; " , vector[i]);
         }
@@ -44,42 +43,40 @@ public class Vector {
     }
 
 
-    public static void binaryInsertionSort (Vector vector, int length) throws Exception{
+
+    public static void binaryInsertionSort (Vector  vector, int length) throws Exception{
         int order = 1;
 
         while(order<length){
-            double helper = vector.vector[order-1];
+            double helper = vector.vector[order];
             int location = binarySearch(vector,order,helper);
-            if(helper != vector.vector[location]){
-                String error = String.format(" the object %f is different to %f in %d ", helper, vector.vector[location],location);
-                throw new Exception(error);
+            int iteration = order;
+            while((iteration>location)) {
+                vector.vector[iteration] = vector.vector[iteration-1];
+                iteration--;
             }
-
-            int iteration = order-1;
-            if(vector.vector[location]>vector.vector[location+1]){
-                while((iteration>= 0) && vector.vector[iteration+1]<=vector.vector[iteration]){
-                    double aux = vector.vector[iteration];
-                    vector.vector[iteration] = vector.vector[iteration+1];
-                    vector.vector[iteration+1] = aux;
-                    iteration--;
-                }
-            }
+            vector.vector[location] = helper;
             order++;
         }
     }
 
 
-    public static void binaryInsertionSortR(Vector vector, int position){
-        if(position == 0)
+    public static void binaryInsertionSortR(Vector vector, int position) throws Exception {
+        if(position>=vector.length){
             return;
-        int j = position-1;
-        double helper = vector.vector[j];
-        while((j>0) && (vector.vector[j-1] > helper)){
-            vector.vector[j] = vector.vector[j-1];
-            j -= 1;
         }
-        vector.vector[j] = helper;
-        binaryInsertionSortR(vector,j);
+
+        double helper = vector.vector[position];
+        int location = binarySearchR(vector,helper,0,position);
+
+        int iteration = position -1;
+            while((iteration>location)) {
+                vector.vector[iteration] = vector.vector[iteration-1];
+                iteration--;
+            }
+        vector.vector[location] = helper;
+        binaryInsertionSortR(vector,position+1);
+
     }
 
 
@@ -89,12 +86,11 @@ public class Vector {
         if(length == 1 && vector.vector[length] == key){
             return 0;
         }
-
+        int middle;
         int floor = 0;
         int top = length-1;
-
         while(floor <= top){
-            int middle = (floor+top)/2;
+            middle = (floor+top)/2;
             if (key == vector.vector[middle])
                 return middle;
             if(key > vector.vector[middle])
@@ -102,15 +98,13 @@ public class Vector {
             else
                 top = middle - 1;
         }
-        String error = String.format("Error: Not found %f in vector",key);
-        throw new Exception(error);
+        return top+1;
     }
 
     public static int binarySearchR(Vector vector, double key, int floor, int top) throws Exception {
         int middle = (floor+top)/2;
         if(floor>top){
-            String error = String.format("Error: Not found %f in vector",key);
-            throw new Exception(error);
+            return top+1;
         }
         if(key == vector.vector[middle])
             return middle;
@@ -137,8 +131,21 @@ public class Vector {
         }
     }
 
-    public static void insertionSort(Vector vector){
+    public static void insertionSortR(Vector vector,int length){
+        if(length<=1){
+            return;
+        }
+        insertionSortR(vector,length-1);
 
+        double last = vector.vector[length-1];
+        int j = length-2;
+
+        while(j >= 0 && vector.vector[j] > last){
+            vector.vector[j+1] = vector.vector[j];
+            j--;
+        }
+
+        vector.vector[j+1] = last;
     }
 
 
